e0661b0633845b9da220dd99467bf80c29d5c734
ZKTeca <102985602+ZKTeca@users.noreply.github.com>
2023-02-27 21:48:22 +0000

    Imported folder structure

--- llm/openai/chatgpt.go
+++ llm/openai/chatgpt.go
@@ -14,3 +14,17 @@ type ChatGPT struct {
 	model  string
 	client *openai.Client
 }
+
+type Option func(c *ChatGPT)
+
+func WithModel(model string) Option {
+	return func(c *ChatGPT) {
+		c.model = model
+	}
+}
+
+func NewChatGPT(token string, opts ...Option) *ChatGPT {
+
+	client := openai.NewClient(token)
+
+	chatgpt := &ChatGPT{
