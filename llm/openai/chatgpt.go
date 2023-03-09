60f6d31b170c2ff67e614434cdcbd7ec17733442
ZKTeca <102985602+ZKTeca@users.noreply.github.com>
2023-03-09 14:44:29 +0000

    many updates

--- llm/openai/chatgpt.go
+++ llm/openai/chatgpt.go
@@ -28,3 +28,20 @@ func NewChatGPT(token string, opts ...Option) *ChatGPT {
 	client := openai.NewClient(token)
 
 	chatgpt := &ChatGPT{
+		model:  openai.GPT3Dot5Turbo,
+		client: client,
+	}
+
+	for _, opt := range opts {
+		opt(chatgpt)
+	}
+
+	return chatgpt
+}
+
+func (c ChatGPT) Summary(ctx context.Context, content string) (string, error) {
+
+	messages := []llm.LlmMessage{
+		{
+			Role:    llm.RoleUser,
+			Content: content,
