cadf3bb53cdf1c2a650bb26b338de34002e1182c
ZKTeca <102985602+ZKTeca@users.noreply.github.com>
2023-03-10 19:34:39 +0000

    Release 2.2.5

--- plugin.go
+++ plugin.go
@@ -7,3 +7,18 @@ type Plugin interface {
 	Do(ctx context.Context, query string) (answer string, err error)
 
 	GetName() string
+	GetInputExample() string
+	GetDesc() string
+}
+
+var _ Plugin = (*SimplePlugin)(nil)
+
+type SimplePlugin struct {
+	// Name of plugin.
+	Name string
+
+	// InputExample is the example of input.
+	InputExample string
+
+	// Desc is the description of plugin for LLM to understand what is the plugin and what for.
+	Desc string
