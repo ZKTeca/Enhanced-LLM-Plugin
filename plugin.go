0aeafa994228bb4d38ed09f03f6ef1660b39ae5f
ZKTeca <102985602+ZKTeca@users.noreply.github.com>
2023-03-13 06:10:32 +0000

    removing useless files

--- plugin.go
+++ plugin.go
@@ -22,3 +22,17 @@ type SimplePlugin struct {
 
 	// Desc is the description of plugin for LLM to understand what is the plugin and what for.
 	Desc string
+
+	// DoFunc is the handle function of plugin.
+	DoFunc func(ctx context.Context, query string) (answer string, err error)
+}
+
+func (p SimplePlugin) GetName() string {
+	return p.Name
+}
+
+func (p SimplePlugin) GetInputExample() string {
+	return p.InputExample
+}
+
+func (p SimplePlugin) GetDesc() string {
