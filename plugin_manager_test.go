dcaa21c1a2b008ff06ffc767066ec2fe55679fa0
ZKTeca <102985602+ZKTeca@users.noreply.github.com>
2023-04-21 20:08:56 +0000

    improved logging

--- plugin_manager_test.go
+++ plugin_manager_test.go
@@ -10,3 +10,11 @@ import (
 	"github.com/agi-cn/llmplugin/llm/openai"
 	"github.com/agi-cn/llmplugin/plugins/calculator"
 	"github.com/agi-cn/llmplugin/plugins/google"
+	"github.com/agi-cn/llmplugin/plugins/stablediffusion"
+	"github.com/joho/godotenv"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestManagerSelectPlugin(t *testing.T) {
