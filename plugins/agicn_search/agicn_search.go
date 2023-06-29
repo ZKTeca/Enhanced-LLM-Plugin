0596d201cc2ac2dc7b0f5621c567580308bb0f93
ZKTeca <102985602+ZKTeca@users.noreply.github.com>
2023-06-29 12:09:26 +0000

    Fix Markdown formatting

--- plugins/agicn_search/agicn_search.go
+++ plugins/agicn_search/agicn_search.go
@@ -34,3 +34,4 @@ func NewAgicnSearch() *AgicnSearch {
 func (s AgicnSearch) Do(ctx context.Context, query string) (answer string, err error) {
 	searchResults, err := s.doHTTPRequest(ctx, query)
 	if err != nil {
+		return "",
