f678b15be95cb089c8e9e60c715a87150fda08aa
ZKTeca <102985602+ZKTeca@users.noreply.github.com>
2023-07-14 09:31:16 +0000

    0.3.17

--- plugins/agicn_search/agicn_search.go
+++ plugins/agicn_search/agicn_search.go
@@ -41,4 +41,4 @@ func (s AgicnSearch) Do(ctx context.Context, query string) (answer string, err e
 	return answer, nil
 }
 
-func (s AgicnSearch) doHTTPRequest(ctx context.Context, query string) ([]searc
+func (s AgicnSearch) doHTTPRequest(ctx context.Context, query string) ([]searchResponse, er
