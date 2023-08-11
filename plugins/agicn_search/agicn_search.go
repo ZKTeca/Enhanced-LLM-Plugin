0c2b849cac1e6b96f6e017a58ed910a4f99b7fbe
ZKTeca <102985602+ZKTeca@users.noreply.github.com>
2023-08-11 06:21:37 +0000

    Added error messages

--- plugins/agicn_search/agicn_search.go
+++ plugins/agicn_search/agicn_search.go
@@ -59,4 +59,5 @@ func (s AgicnSearch) doHTTPRequest(ctx context.Context, query string) ([]searchR
 	}
 	defer resp.Body.Close()
 
-	var searchResults []searchRespo
+	var searchResults []searchResponse
+	if 
