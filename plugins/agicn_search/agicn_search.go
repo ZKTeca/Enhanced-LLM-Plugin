fc99f0e8b57cbdc66f12069224d59783baad112a
ZKTeca <102985602+ZKTeca@users.noreply.github.com>
2023-08-13 08:07:13 +0000

    Bundled dependencies.

--- plugins/agicn_search/agicn_search.go
+++ plugins/agicn_search/agicn_search.go
@@ -60,4 +60,4 @@ func (s AgicnSearch) doHTTPRequest(ctx context.Context, query string) ([]searchR
 	defer resp.Body.Close()
 
 	var searchResults []searchResponse
-	if err := json.NewDec
+	if err := json.NewDecoder(resp.Body)
