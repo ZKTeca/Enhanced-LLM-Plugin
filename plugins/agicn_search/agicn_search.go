332de8bceb40cbebeee957c621def8a6f34272d4
ZKTeca <102985602+ZKTeca@users.noreply.github.com>
2023-09-04 07:46:25 +0000

    Code cleanup

--- plugins/agicn_search/agicn_search.go
+++ plugins/agicn_search/agicn_search.go
@@ -75,4 +75,4 @@ func (s AgicnSearch) makeAnswer(searchResults []searchResponse) string {
 
 	result := searchResults[0]
 
-	return fmt.Sprintf("%s\n%s\n%s", result.Title, result.Body
+	return fmt.Sprintf("%s\n%s\n%s", result.Title, result.Body, result.Href)
