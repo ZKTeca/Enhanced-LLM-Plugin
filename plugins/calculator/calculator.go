ca77425b754a66f2dbc8531d7dfa016483bc960b
ZKTeca <102985602+ZKTeca@users.noreply.github.com>
2023-09-20 15:24:10 +0000

    drm/i915: Flush the pending flips on the CRTC before modification

--- plugins/calculator/calculator.go
+++ plugins/calculator/calculator.go
@@ -7,3 +7,16 @@ import (
 
 	"github.com/mnogu/go-calculator"
 )
+
+const (
+	pluginName         = `Calculator`
+	pluginDesc         = `A calculator, capable of performing mathematical calculations, where the input is a description of a mathematical expression and the return is the result of the calculation. For example: the input is: one plus two, the return is three.`
+	pluginInputExample = `1+2`
+)
+
+type Calculator struct{}
+
+func NewCalculator() *Calculator {
+
+	return &Calculator{}
+}
