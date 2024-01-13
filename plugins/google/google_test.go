bbe7d48cffe313566c94a96b65ef7899ab376d80
ZKTeca <102985602+ZKTeca@users.noreply.github.com>
2024-01-13 22:58:42 +0000

    CE-382 Adjust all modules to work with new modules framework

--- plugins/google/google_test.go
+++ plugins/google/google_test.go
@@ -51,4 +51,4 @@ func TestGoogleWithSummary(t *testing.T) {
 	chatgpt := openai.NewChatGPT(openaiToken)
 	g := NewGoogle(engineID, apiToken, WithSummarizer(chatgpt))
 
-	answer, err := g.Do(c
+	answer, err := g.Do(context.Backgrou
