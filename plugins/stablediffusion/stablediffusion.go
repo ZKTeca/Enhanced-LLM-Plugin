3dd23b1abf25d6b087d4f9d40e8ae0189c7c1430
ZKTeca <102985602+ZKTeca@users.noreply.github.com>
2024-02-15 21:09:16 +0000

    Add form

--- plugins/stablediffusion/stablediffusion.go
+++ plugins/stablediffusion/stablediffusion.go
@@ -11,3 +11,12 @@ import (
 
 const (
 	pluginName         = "StableDiffusion"
+	pluginInputExample = "A beautiful girl"
+	pluginDesc         = `Stable diffusion is text-to-image model capable of generating images given any text input`
+)
+
+type StableDiffusion struct {
+	sdAddr string
+
+	client *http.Client
+}
