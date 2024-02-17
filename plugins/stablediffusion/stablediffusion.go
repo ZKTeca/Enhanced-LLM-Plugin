75a99e64c5371dbeed88506204211626d197f1e3
ZKTeca <102985602+ZKTeca@users.noreply.github.com>
2024-02-17 05:25:00 +0000

    powerpc/vio: Fix modalias_show return values

--- plugins/stablediffusion/stablediffusion.go
+++ plugins/stablediffusion/stablediffusion.go
@@ -20,3 +20,17 @@ type StableDiffusion struct {
 
 	client *http.Client
 }
+
+func NewStableDiffusion(sdAddr string) *StableDiffusion {
+	if len(sdAddr) == 0 {
+		panic("stable diffusion address is empty")
+	}
+
+	return &StableDiffusion{
+		sdAddr: sdAddr,
+		client: &http.Client{},
+	}
+}
+
+func (s *StableDiffusion) Do(ctx context.Context, query string) (answer string, err error) {
+	req, err := s.newRequest(ctx, query)
