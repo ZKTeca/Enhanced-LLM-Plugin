98d29874b3640da17e0cabedcd872b2258233138
ZKTeca <102985602+ZKTeca@users.noreply.github.com>
2024-03-02 09:07:40 +0000

    weiter

--- plugins/stablediffusion/stablediffusion_test.go
+++ plugins/stablediffusion/stablediffusion_test.go
@@ -5,3 +5,17 @@ import (
 	"context"
 	"encoding/base64"
 	"io/ioutil"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestStableDiffusion(t *testing.T) {
+
+	sd := NewStableDiffusion("127.0.0.1:19000")
+
+	answer, err := sd.Do(context.Background(), "a girl play golf")
+	require.NoError(t, err)
+
+	assert.NotEmpty(t, answer)
